6924a04391e7cd086a1bd3345ba4fcb635330521
zawminsoe <alinyaung225800@gmail.com>
2024-11-10 16:07:16 +0630

    first commit

---
+++ asset/app.js
@@ -0,0 +1,60 @@
+let selector = (ele) => document.querySelector(ele);
+let allSelector = (ele) => document.querySelectorAll(ele);
+
+const inputText = selector(".todo-input");
+const addBtn = selector(".addBtn");
+const todoItem = selector(".todo-item");
+
+document.addEventListener("DOMContentLoaded", loadTodos);
+addBtn.addEventListener("click", addTodoList);
+
+function addTodoList(e) {
+  let inputVal = inputText.value;
+  if (inputVal.trim() == "") {
+    return;
+  }
+
+  const todo = createElement(inputVal);
+  todoItem.appendChild(todo);
+  saveTodos();
+  inputText.value = "";
+}
+function createElement(value) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+  <span>${value}</span>
+  <button class="delete-btn">Delete</button>
+  `;
+  li.querySelector("button").addEventListener("click", deleteTodo);
+  return li;
+}
+function deleteTodo(e) {
+  let parentELement = e.target.parentElement;
+  parentELement.remove();
+  saveTodos();
+}
+
+function saveTodos() {
+  const todos = [];
+  let todoItems = document.querySelectorAll(".todo-item li span");
+
+  todoItems.forEach((todo) => {
+    console.log(todo);
+    todos.push(todo.textContent);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadTodos() {
+  const storedTodos = localStorage.getItem("todos");
+
+  if (storedTodos) {
+    const todos = JSON.parse(storedTodos);
+
+    todos.forEach((todo) => {
+      const newTodo = createElement(todo);
+
+      todoItem.appendChild(newTodo);
+    });
+  }
+}
